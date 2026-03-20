5ef05caf4c326126f1881aae673af2ac0eaee010
KIRAN C <89858682+Kiranc9901@users.noreply.github.com>
2026-03-20 20:30:40 +0530

    Create Average Selling Price.sql

---
+++ Average Selling Price.sql
@@ -0,0 +1,11 @@
+select
+ p.product_id,
+ round(
+    ifnull(sum(p.price * u.units)/ sum(u.units),0),
+  2)
+  as average_price
+FROM Prices p 
+LEFT JOIN UnitsSold u
+on p.product_id = u.product_id 
+and u.purchase_date between p.start_date and p.end_date
+group by product_id;
